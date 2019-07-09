adadb389d541a8ec7714eb97ef7dc546fde3f38e
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2019-07-09 12:34:24 +0700

    Use asyncio for saving

--- dego_tipbot/dego_bot_sql_update_balances.py
+++ dego_tipbot/dego_bot_sql_update_balances.py
@@ -3,15 +3,17 @@ import sys
 from config import config
 import store
 import time
+import asyncio
 
-### Let's run balance update by a separate process
-while True:
+
+# Let's run balance update by a separate process
+async def update_balance():
     print('sleep in second: 5 seconds')
     while True:
         time.sleep(5)
         start = time.time()
         try:
-            store.sql_update_balances()
+            await store.sql_update_balances()
             end = time.time()
             print('Done update balance: duration (s): '+str(end - start))
             print('Sleep in second: '+str(config.wallet_balance_update_interval))
@@ -20,3 +22,7 @@ while True:
             print('Time out.. try again in 15s..')
             time.sleep(15)
             print(e)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(update_balance())
+loop.close()
